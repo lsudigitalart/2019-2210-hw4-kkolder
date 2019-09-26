5533f1430ed3263abab782dd9370a570ac0b36c5
Kase <54596979+kkolder@users.noreply.github.com>
2019-09-26 13:50:19 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+var xBall = Math.floor(Math.random() * 300) + 50;
+var yBall = 50;
+var diameter = 50;
+
+var xBallChange = 5;
+var yBallChange = 5;
+
+var xPaddle;
+var yPaddle;
+var paddleWidth = 100;
+var paddleHeight = 25;
+
+var started = false;
+
+var score = 0;
+
+function setup() {
+    createCanvas(800,600);
+  }
+  function draw() {
+    background(100);
+fill(255, 5, 140);
+noStroke();
+ellipse(xBall, yBall, diameter, diameter);
+xBall += xBallChange;
+yBall += yBallChange;
+if (xBall < diameter/2 || 
+    xBall > 800 - 0.5*diameter) {
+xBallChange *= -1;
+}
+if (yBall < diameter/2 || 
+   yBall > 600 - diameter) {
+yBallChange *= -1;
+}
+if ((xBall > xPaddle &&
+    xBall < xPaddle + paddleWidth) &&
+    (yBall + (diameter/2) >= yPaddle)) {
+xBallChange *= -1;
+yBallChange *= -1;
+score++;
+}
+if (!started) {
+    xPaddle = windowWidth / 2;
+    yPaddle = windowHeight - 100;
+    started = true;
+  }
+  fill(0, 255, 255);
+noStroke();
+rect(xPaddle, yPaddle, paddleWidth, paddleHeight);
+fill(0, 255, 255);
+textSize(24);
+text("Score: " + score, 10, 25);
+  }
+  function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    xPaddle -= 50;
+  } else if (keyCode === RIGHT_ARROW) {
+    xPaddle += 50;
+  }
+}
